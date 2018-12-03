7fcae4e58e6d7803bd93ec913090c77843e3aa3f
耐小心 <qiqizjl@qq.com>
2018-12-03 11:03:36 +0800

    update

--- index.d.ts
+++ index.d.ts
@@ -1,28 +1,25 @@
 import { Redis, RedisOptions } from "ioredis";
 
-interface RedisOpts extends RedisOptions {
-    password?: string | undefined | null;
-}
-
-interface ClusterOptions extends RedisOpts {
-    cluster?: boolean;
-    nodes?: RedisOpts[];
+interface ClusterOptions extends RedisOptions {
+  cluster?: boolean;
+  nodes?: RedisOptions[];
 }
 
 interface EggRedisOptions {
-    default?: object;
-    app?: boolean;
-    agent?: boolean;
-    client?: ClusterOptions;
-    clients?: Record<string, RedisOpts>;
+  Redis?: Redis;
+  default?: object;
+  app?: boolean;
+  agent?: boolean;
+  client?: ClusterOptions;
+  clients?: Record<string, RedisOptions>;
 }
 
 declare module 'egg' {
-    interface Application {
-        redis: Redis;
-    }
+  interface Application {
+    redis: Redis;
+  }
 
-    interface EggAppConfig {
-        redis: EggRedisOptions;
-    }
+  interface EggAppConfig {
+    redis: EggRedisOptions;
+  }
 }
